b5d678a44f75680f4047ae676012656c3e7c60c5
Chris Hansen <cthansen22@gmail.com>
2014-06-04 08:21:37 -0700

    Initial Commit

---
+++ CTCalculator/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CTCalculator
+//
+//  Created by Chris Hansen on 6/4/14.
+//  Copyright (c) 2014 Chris Hansen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+                            
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
